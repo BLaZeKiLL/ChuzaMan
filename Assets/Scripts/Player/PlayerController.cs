c43beddfd48bbc9c93461287d38d6ca05b0ec124
Devashish Lal <laldevashish@gmail.com>
2020-06-29 21:31:16 +0530

    Basic Movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chuzaman.Player {
+
+    public class PlayerController : MonoBehaviour {
+
+        [SerializeField] private float _speed = 10f;
+        
+        private Vector2 _direction;
+        private Rigidbody2D _rigidbody;
+        
+        private void Start() {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update() {
+            if (_rigidbody.velocity == Vector2.zero) {
+                GetInput();
+            }
+            _rigidbody.velocity = _direction * _speed;
+        }
+
+        private void GetInput() {
+            if (Input.GetKeyDown(KeyCode.W)) {
+                _direction = Vector2.up;
+            } else if (Input.GetKeyDown(KeyCode.S)) {
+                _direction = Vector2.down;
+            } else if (Input.GetKeyDown(KeyCode.A)) {
+                _direction = Vector2.left;
+            } else if (Input.GetKeyDown(KeyCode.D)) {
+                _direction = Vector2.right;
+            } else {
+                _direction = Vector2.zero;
+            }
+        }
+
+    }
+
+}
